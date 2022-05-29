3966535d01d8430d4b149b5ca8b222ec7f622b8e
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-05-28 23:00:26 -0400

    working api

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@
 #[macro_use]
 extern crate rocket;
 
-use chrono;
+use rocket::Request;
+use rocket::serde::{Serialize, Deserialize};
+use rocket::serde::json::Json;
 
-use rocket::http::Status;
-use rocket::response::status;
+use reqwest::StatusCode;
+
+use chrono;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -18,33 +21,48 @@ fn datetime() -> String {
 }
 
 const DEFINE_EN_API_URL: &'static str = "https://api.dictionaryapi.dev/api/v2/entries/en";
-#[get("/")]
-async fn define() -> Status {
-    define_en_handler("poop").await.unwrap();
-    Status::Ok
+#[get("/en/<word>")]
+async fn define(word: &str) -> Json<Vec<DictionaryEntry>> {
+    let definitions = define_en_handler(word).await.unwrap();
+    Json(definitions)
 }
 
-async fn define_en_handler(word: &str) -> Result<(), reqwest::Error> {
+// TODO make macro to auto make structs from json
+#[derive(Debug, Serialize, Deserialize)]
+struct DictionaryEntry {
+    word: String,
+    meanings: Vec<Meaning>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct Meaning {
+    partOfSpeech: String,
+    definitions: Vec<Definition>,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct Definition {
+    definition: String,
+}
 
-    println!("en handler");
-    /*
-    let client = reqwest::Client::builder()
-        .timeout(std::time::Duration::from_millis(100))
-        .build()?;
-    let resp = client
-        .get(format!("{}/{}", DEFINE_EN_API_URL, word))
-        .send().await?;
-    */
+async fn define_en_handler(word: &str) -> Result<Vec<DictionaryEntry>, reqwest::Error> {
+
     let resp = reqwest::get(format!("{}/{}", DEFINE_EN_API_URL, word)).await?;
-    println!("response {:?}", resp);
-    Ok(())
+    if resp.status() != StatusCode::OK {
+        return Ok(Vec::new());
+    }
+    let definitions: Vec<DictionaryEntry> = resp.json().await?;
+    Ok(definitions)
 }
 
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("not found :(")
+}
 
 #[launch]
 fn launch() -> _ {
     rocket::build()
         .mount("/datetime", routes![datetime])
         .mount("/define", routes![define])
+        .register("/", catchers![not_found])
 }
 
